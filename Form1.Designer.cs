064fb7283ca61238aa402bce6aad94b77daaef72
Bhaskar Acharya <bhaskar.acharya@adapty.com>
2022-01-15 22:33:28 +0530

    First Check-In

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace RepoTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(504, 597);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 610);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(504, 678);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GitHub Repository Structure";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RepoTree
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTree();
+        }
+
+        public void InitializeTree()
+        {
+            ReadExcel();
+            TreeNode root = new TreeNode("DigitalTech     ");
+            root.NodeFont = new Font(treeView1.Font, FontStyle.Bold);
+            treeView1.Nodes.Add(root);
+            CreateTree("1", root);
+            treeView1.ExpandAll();
+        }
+
+
+        private void CreateTree(string ParentId, TreeNode tn)
+        {
+            var result = from s in listRepoItem where s.ParentId == ParentId select s;
+            foreach(RepoItem x in result)
+            {
+                TreeNode newTn = new TreeNode(x.DisplayText);
+                if (x.RepoType == "Repo") { newTn.ForeColor = Color.Blue; }
+                tn.Nodes.Add(newTn);
+                CreateTree(x.Id, newTn);
+            }
+        }
+
+        public class RepoItem
+        {
+            public string Id;
+            public string DisplayText;
+            public string RepoType;
+            public string ParentId;
+        }
+
+        public Collection<RepoItem> listRepoItem;
+        private void ReadExcel()
+        {
+            OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=.\RepoTree.xlsx;Extended Properties='Excel 12.0;HDR=YES;'");
+            try
+            {
+                conn.Open();
+                string sqlQuery = "SELECT * FROM [Repo$] WHERE Active=1";
+                OleDbCommand cmdRepo = new OleDbCommand(sqlQuery, conn);
+                OleDbDataReader drRepo = cmdRepo.ExecuteReader();
+                listRepoItem = new Collection<RepoItem>();
+                while (drRepo.Read())
+                {
+                    RepoItem repoItem = new RepoItem()
+                    {
+                        Id = drRepo["ID"].ToString(),
+                        DisplayText = drRepo["Name"].ToString(),
+                        RepoType = drRepo["Type"].ToString(),
+                        ParentId = drRepo["ParentID"].ToString(),
+                    };
+                    listRepoItem.Add(repoItem);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();    
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            InitializeTree();
+        }
+    }
+}
